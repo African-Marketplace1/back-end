7aa9a06642948f528fa2fd38f8347b6e0a34faf4
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-09-20 17:38:26 -0400

    register endpoint works, login endpoint works (returns a json web token)

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const Users = require("./users-model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
+const JWT_SECRET = process.env.JWT_SECRET || "shh";
 
 router.get("/:id", async (req, res, next) => {
   const { id } = req.params;
@@ -10,13 +13,41 @@ router.get("/:id", async (req, res, next) => {
 
 router.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
-  const user = await Users.getBy({ username: username });
-  res.status(200).json(user);
+  try {
+    const user = await Users.getBy({ username });
+    if (bcrypt.compareSync(password, user.password)) {
+      const token = buildToken(user);
+      res
+        .status(200)
+        .json({ message: `Welcome back ${user.username}`, token: token });
+    } else {
+      next({ status: 401, message: "Invalid Credentials" });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post("/register", async (req, res, next) => {
-  const user = await Users.add(req.body);
-  res.status(200).json(user);
+  const { password } = req.body;
+  const hash = bcrypt.hashSync(password, 8);
+  try {
+    const newUser = await Users.add({ ...req.body, password: hash });
+    res.status(200).json(newUser);
+  } catch (err) {
+    next(err);
+  }
 });
+
+const buildToken = (user) => {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: "2d",
+  };
+  return jwt.sign(payload, JWT_SECRET, options);
+};
 
 module.exports = router;
