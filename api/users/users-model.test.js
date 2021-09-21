6495ed5ebac2c572968f1577790057ef4ed0e67d
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-09-21 11:32:57 -0400

    made tests for users model functions

--- api/users/users-model.test.js
+++ api/users/users-model.test.js
@@ -20,9 +20,9 @@ describe("users-model", () => {
   it("is the correct testing environment", async () => {
     expect(process.env.NODE_ENV).toBe("testing");
   });
-  describe("getById", () => {
+  describe("getUserById", () => {
     it("returns an object with username, password", async () => {
-      const response = await User.getById(1);
+      const response = await User.getUserById(1);
       const username = response.username;
       expect(username).toBe("bmenz");
       expect(response).toHaveProperty("username");
@@ -55,8 +55,128 @@ describe("users-model", () => {
           },
         ],
       };
-      const actual = await User.getById(2);
+      const actual = await User.getUserById(2);
       expect(actual).toEqual(expected);
+    });
+  });
+  describe("getUserBy", () => {
+    test("returns correct properties when searching by username", async () => {
+      const response = await User.getUserBy({ username: "bmenz" });
+      expect(response).toHaveProperty("username");
+      expect(response).toHaveProperty("password");
+      expect(response).toHaveProperty("email");
+      expect(response).toHaveProperty("location");
+    });
+    test("returns correct properties when searching by email", async () => {
+      const response = await User.getUserBy({ email: "ab@gmail.com" });
+      expect(response).toHaveProperty("username");
+      expect(response).toHaveProperty("password");
+      expect(response).toHaveProperty("email");
+      expect(response).toHaveProperty("location");
+    });
+    test("does not return a user on incorrect username", async () => {
+      const response = await User.getUserBy({ username: "fakeUser" });
+      expect(response).toBeFalsy();
+    });
+  });
+
+  describe("addUser", () => {
+    test("adds a new user with username, password, and email", async () => {
+      const newUser = {
+        username: "user",
+        password: "pass",
+        email: "user@test.com",
+      };
+      await User.addUser(newUser);
+      const users = await db("users");
+      expect(users).toHaveLength(4);
+    });
+    test("returns the new user", async () => {
+      const expected = {
+        user_id: 4,
+        username: "test",
+        password: "test",
+        email: "test@test.com",
+        location: "123 tulip lane",
+        img: null,
+      };
+      const newUser = {
+        username: "test",
+        password: "test",
+        email: "test@test.com",
+        location: "123 tulip lane",
+      };
+      const response = await User.addUser(newUser);
+      expect(response).toEqual(expected);
+    });
+  });
+  describe("addProduct", () => {
+    test("adds a new product", async () => {
+      const newProduct = {
+        name: "banana",
+        price_usd: 1.0,
+        description: "good",
+      };
+      await User.addProduct(1, 2, newProduct);
+      const products = await db("products");
+      expect(products).toHaveLength(6);
+    });
+    test("returns all the sellers products", async () => {
+      const expected = [
+        {
+          product_id: 1,
+          name: "Eggs",
+          price_usd: 5.99,
+          description: "12 per pack",
+          seller: 1,
+          img: "https://solidstarts.com/wp-content/uploads/when-can-babies-eat-eggs-480x320@2x.webp",
+          category: 1,
+        },
+        {
+          product_id: 2,
+          name: "Avocado",
+          price_usd: 2.99,
+          description: "1 avocado per purchase",
+          seller: 1,
+          img: "https://www.washingtonian.com/wp-content/uploads/2020/02/iStock-1027572462-scaled-2048x1695.jpg",
+          category: 4,
+        },
+        {
+          product_id: 3,
+          name: "Beans Rosecoco",
+          price_usd: 6.5,
+          description: "2 lbs per bag",
+          seller: 1,
+          img: "https://assets.sainsburys-groceries.co.uk/gol/7693115/1/640x640.jpg",
+          category: 2,
+        },
+        {
+          product_id: 6,
+          name: "banana",
+          price_usd: 1,
+          description: "good",
+          seller: 1,
+          img: null,
+          category: 4,
+        },
+      ];
+      const newProduct = {
+        name: "banana",
+        price_usd: 1.0,
+        description: "good",
+      };
+      const response = await User.addProduct(1, 4, newProduct);
+      expect(response).toEqual(expected);
+    });
+  });
+  describe("getCategoryByName", () => {
+    test("returns category id when inputting its name (Fruits)", async () => {
+      const response = await User.getCategoryByName("Fruits");
+      expect(response).toBe(4);
+    });
+    test("returns category id when inputting its name (Animal Products)", async () => {
+      const response = await User.getCategoryByName("Animal Products");
+      expect(response).toBe(1);
     });
   });
 });
