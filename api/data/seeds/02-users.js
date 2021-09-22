41dc2abea0c2f7c58ff10ff6ce739d45fbfa6789
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-09-22 16:12:25 -0400

    changed seeds

--- api/data/seeds/02-users.js
+++ api/data/seeds/02-users.js
@@ -1,21 +1,23 @@
+const bcrypt = require("bcryptjs");
+
 const users = [
   {
     username: "bmenz",
-    password: "abc123",
+    password: bcrypt.hashSync("pass", 8),
     email: "bmenz@gmail.com",
     location: "529W+7X Harare, Zimbabwe",
     img: "https://images.pexels.com/photos/3785991/pexels-photo-3785991.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   },
   {
     username: "ab_caloo",
-    password: "abc456",
+    password: bcrypt.hashSync("pass", 8),
     email: "ab@gmail.com",
     location: "567M+V8 Luanda, Angola",
     img: "https://images.pexels.com/photos/6194365/pexels-photo-6194365.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   },
   {
     username: "hfgool",
-    password: "gool123",
+    password: bcrypt.hashSync("pass", 8),
     email: "gool@gmail.com",
     location: "8HQ8+XQ Swakopmund, Namibia",
     img: "https://images.pexels.com/photos/5082976/pexels-photo-5082976.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
